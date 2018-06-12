375ab6ec5d74ebcbd51913d07a46b3f536c8560d
linhnh5015 <linh.nh5015@gmail.com>
2018-06-13 02:37:38 +0700

    Fixed

--- forward_backward.py
+++ forward_backward.py
@@ -1,70 +1,68 @@
+from builtins import enumerate
+
 from hmm import HMM
 import numpy as np
 import json
 
 
-def backward(hmm, emissions):
-    dist = uniform(hmm.num_states)
-    dists = [dist]
+def forward_backward(obs, transition, emission, initial):  # returns model given the initial model and observations
+    num_states = len(initial)
+    fwd = [[0 for i in range(num_states)] for j in range(len(obs))]
+    # Initialize base cases (t == 0)
+    for y in range(num_states):
+        fwd[0][y] = initial[y] * emission[y][obs[0]]
+    # Run Forward algorithm for t > 0
+    for t in range(1, len(obs)):
+        for y in range(num_states):
+            fwd[t][y] = sum((fwd[t - 1][y0] * transition[y0][y] * emission[y][obs[t]]) for y0 in range(num_states))
+    bwk = [[0 for i in range(num_states)] for j in range(len(obs))]
+    T = len(obs)
+    # Initialize base cases (t == T)
+    for y in range(num_states):
+        bwk[T - 1][y] = 1
+    for t in reversed(range(T - 1)):
+        for y in range(num_states):
+            bwk[t][y] = sum((bwk[t + 1][y1] * transition[y][y1] * emission[y1][obs[t + 1]]) for y1 in range(num_states))
+    p_obs = sum((initial[y] * emission[y][obs[0]] * bwk[0][y]) for y in range(num_states))
+    gamma = [[0 for i in range(num_states)] for j in range(len(obs))]
+    zi = [[[0 for i in range(num_states)] for j in range(num_states)] for k in range(len(obs))]
 
-    for emission in reversed(emissions):
-        dist = backward_step(hmm, dist, emission)
-        dists.append(dist)
+    for t in range(len(obs)):
+        for y in range(num_states):
+            gamma[t][y] = (fwd[t][y] * bwk[t][y]) / p_obs
+            if t == 0:
+                initial[y] = gamma[t][y]
+            # compute zi values up to T - 1
+            if t == len(obs) - 1:
+                continue
+            for y1 in range(num_states):
+                zi[t][y][y1] = fwd[t][y] * transition[y][y1] * emission[y1][obs[t + 1]] * bwk[t + 1][y1] / p_obs
 
-    dists.reverse()
-
-    return np.row_stack(dists)
-
-
-def backward_step(hmm, dist, emission):
-    return normalize(np.dot(hmm.transition_probs, np.dot(np.diagflat(hmm.emission_dist(emission)), dist.T)).T)
-
-
-def forward_backward(hmm, initial_dist, emissions):
-    forward_dists = forward(hmm, initial_dist, emissions)
-    backward_dists = backward(hmm, emissions)
-
-    return normalize(np.multiply(forward_dists, backward_dists))
-
-
-def forward(hmm, initial_dist, emissions):
-    dist = initial_dist
-    dists = [dist]
-
-    for emission in emissions:
-        dist = forward_step(hmm, dist, emission)
-        dists.append(dist)
-
-    return np.row_stack(dists)
-
-
-def forward_step(hmm, dist, emission):
-    return normalize(np.dot(dist, np.dot(hmm.transition_probs, np.diagflat(hmm.emission_dist(emission)))))
-
-
-def modify_tuple(tuple_, ix, value):
-    as_list = list(tuple_)
-    as_list[ix] = value
-
-    return tuple(as_list)
-
-
-def normalize(array, axis=1):
-    sum_shape = modify_tuple(array.shape, axis, 1)
-    return array / np.reshape(np.sum(array, axis=axis), sum_shape)
-
-
-def uniform(n):
-    return normalize(np.ones((1, n)))
+    for y in range(num_states):
+        for y1 in range(num_states):
+                # we will now compute new a_ij
+            val = sum([zi[t][y][y1] for t in range(len(obs) - 1)])  #
+            val /= sum([gamma[t][y] for t in range(len(obs) - 1)])
+            transition[y][y1] = val
+        # re estimate gamma
+    for y in range(num_states):
+        num_obs = emission.shape[1]
+        for k in range(num_obs):  # for all symbols vk
+            val = 0.0
+            for t in range(len(obs)):
+                if obs[t] == k:
+                    val += gamma[t][y]
+            val /= sum([gamma[t][y] for t in range(len(obs))])
+            emission[y][k] = val
+    return transition, emission, initial
 
 
 if __name__ == "__main__":
-    transmat = np.loadtxt('F:/Hoang Linh/Nam3/Ky_2/stochastic_process/btlon/CodingRegionData/transition')
-    emmat = np.loadtxt('F:/Hoang Linh/Nam3/Ky_2/stochastic_process/btlon/CodingRegionData/emission')
-    initial = [np.loadtxt('F:/Hoang Linh/Nam3/Ky_2/stochastic_process/btlon/CodingRegionData/initial')]
-    with open('F:/Hoang Linh/Nam3/Ky_2/stochastic_process/btlon/CodingRegionData/TestDataSet.json') as f:
+    transmat = np.loadtxt('transition')
+    emmat = np.loadtxt('emission')
+    initial = [np.loadtxt('initial')][0]
+    with open('TestDataSet.json') as f:
         data = json.load(f)
-    emissions = []
     hmm = HMM(transmat, emmat)
     observations = []
     for c in data[1]['Sequence']:
@@ -76,4 +74,15 @@ if __name__ == "__main__":
             observations.append(2)
         else:
             observations.append(3)
-    print(forward_backward(hmm, initial, observations))
+    max_ite = 100
+    count = 0
+    epsilon = 0.1
+    err = 9999
+    while err > epsilon and count < max_ite:
+        transition0, emission0, initial0 = forward_backward(observations, transmat, emmat, initial)
+        err = np.linalg.norm(transmat - transition0, np.inf)
+        err += np.linalg.norm(emmat - emission0, np.inf)
+        err += np.linalg.norm(initial - initial0, np.inf)
+        transmat = transition0
+        emmat = emission0
+        initial = initial0
